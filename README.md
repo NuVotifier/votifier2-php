d8298008a8921d9a5f691ec4877caec70da6b40b
Tux <write@imaginarycode.com>
2017-04-25 06:46:31 -0400

    Converted to streams and added timeouts. Fixes #3

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Votifier protocol v2 client for PHP.
     $vote = new \Imaginarycode\Votifier2\Vote("tuxed", "127.0.0.1", "Test", NULL);
     $service = new \Imaginarycode\Votifier2\Server("127.0.0.1", 8192, "TOKEN");
     $service->sendVote($vote);
+```
--- src/Server.php
+++ src/Server.php
@@ -33,60 +33,53 @@ class Server
             throw new \InvalidArgumentException('vote not provided');
         }
 
-        $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
-        if (!socket_connect($socket, $this->host, $this->port))
+        $stream = stream_socket_client('tcp://' . $this->host . ':' . $this->port, $errno, $errstr, 3);
+        if (!$stream)
         {
-            throw new \ErrorException('Could not connect: ' . socket_strerror(socket_last_error($socket)));
-        }
-        $header = socket_read($socket, 64, PHP_NORMAL_READ);
-
-        if (!$header)
-        {
-            socket_close($socket);
-            throw new \ErrorException('Remote host error: ' . socket_strerror(socket_last_error($socket)));
+            throw new \Exception('Could not connect: ' . $errstr);
         }
 
-        $header_parts = explode(' ', $header);
+        try {
+            stream_set_timeout($stream, 3);
+            $header = fread($stream, 64);
+            if ($header === FALSE) {
+                throw new \Exception("Couldn't read header from remote host");
+            }
+            $header_parts = explode(' ', $header);
 
-        if (count($header_parts) != 3)
-        {
-            socket_close($socket);
-            throw new \ErrorException('Not a Votifier v2 server');
-        }
+            if (count($header_parts) != 3) {
+                throw new \Exception('Not a Votifier v2 server');
+            }
 
-        $challenge = substr($header_parts[2], 0, -1);
+            $challenge = substr($header_parts[2], 0, -1);
 
-        $payload_json = json_encode(array(
-            "username" => $vote->username,
-            "serviceName" => $vote->serviceName,
-            "timestamp" => $vote->timestamp,
-            "address" => $vote->address,
-            "challenge" => $challenge
-        ));
-        $signature = base64_encode(hash_hmac('sha256', $payload_json, $this->token, true));
-        $message_json = json_encode(array("signature" => $signature, "payload" => $payload_json));
+            $payload_json = json_encode(array(
+                "username" => $vote->username,
+                "serviceName" => $vote->serviceName,
+                "timestamp" => $vote->timestamp,
+                "address" => $vote->address,
+                "challenge" => $challenge
+            ));
+            $signature = base64_encode(hash_hmac('sha256', $payload_json, $this->token, true));
+            $message_json = json_encode(array("signature" => $signature, "payload" => $payload_json));
 
-        $payload = pack('nn', 0x733a, strlen($message_json)) . $message_json;
-        if (socket_write($socket, $payload) === FALSE)
-        {
-            socket_close($socket);
-            throw new \ErrorException('Remote host error: ' . socket_strerror(socket_last_error($socket)));
-        }
+            $payload = pack('nn', 0x733a, strlen($message_json)) . $message_json;
+            if (fwrite($stream, $payload) === FALSE) {
+                throw new \Exception("Couldn't write to remote host");
+            }
 
-        $response = socket_read($socket, 256);
+            $response = fread($stream, 256);
 
-        if (!$response)
-        {
-            socket_close($socket);
-            throw new \ErrorException('Remote host error: ' . socket_strerror(socket_last_error($socket)));
-        }
+            if (!$response) {
+                throw new \Exception('Unable to read server response');
+            }
 
-        $result = json_decode($response);
-        socket_close($socket);
-
-        if ($result->status !== "ok")
-        {
-            throw new \ErrorException('Remote server error: ' . $result->cause . ': ' . $result->error);
+            $result = json_decode($response);
+            if ($result->status !== "ok") {
+                throw new \Exception('Votifier server error: ' . $result->cause . ': ' . $result->error);
+            }
+        } finally {
+            fclose($stream);
         }
     }
 }
